34601eee8623561b79b5ea05de55da30c92c6a8e
r32sqbchil <uchyxn@gmail.com>
2023-10-01 20:35:27 +0900

    [1-4] 매니저 만들기
    - MonoBehaviour 설명
    - Managers 스크립트 생성

--- Assets/Scripts/Managers/Managers.cs
+++ Assets/Scripts/Managers/Managers.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class Managers : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,6 @@ public class Test : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(1.0f, 1.0f, 1.0f));
+        
     }
 }
