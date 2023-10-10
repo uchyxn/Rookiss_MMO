9fe496023effbe6c2707103046c73d84bbdab721
r32sqbchil <uchyxn@gmail.com>
2023-10-10 19:21:51 +0900

    [2-4] Vector3
    - Vector3 설명

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,39 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
+struct MyVector
+{
+    public float x;
+    public float y;
+    public float z;
+
+    public float magnitude { get { return Mathf.Sqrt(x * x + y * y + z * z); } }
+    public MyVector normalized { get { return new MyVector(x / magnitude, y / magnitude, z / magnitude); } }
+
+    public MyVector(float x, float y, float z) { this.x = x; this.y = y; this.z = z; }
+
+    public static MyVector operator +(MyVector a, MyVector b)
+    {
+        return new MyVector(a.x + b.x, a.y + b.y, a.z + b.z);
+    }
+    public static MyVector operator -(MyVector a, MyVector b)
+    {
+        return new MyVector(a.x - b.x, a.y - b.y, a.z - b.z);
+    }
+    public static MyVector operator *(MyVector a, float d)
+    {
+        return new MyVector(a.x * d, a.y * d, a.z * d);
+    }
+}
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     float _speed = 10.0f;
     void Start()
     {
-        
+        MyVector to = new MyVector(10.0f, 0.0f, 0.0f);
+        MyVector from = new MyVector(5.0f, 0.0f, 0.0f);
+        MyVector dir = to - from;
+
+        dir = dir.normalized;
+
+        MyVector newPos = from + dir * _speed;
     }
 
     void Update()
     {
-        // Local -> World : TransformDirection
-        // World -> Local : InverseTransformDirection
-
-        //if(Input.GetKey(KeyCode.W))
-        //{
-        //    transform.position += transform.TransformDirection(Vector3.forward * Time.deltaTime * _speed);
-        //}
-        //if(Input.GetKey(KeyCode.S))
-        //{
-        //    transform.position += transform.TransformDirection(Vector3.back * Time.deltaTime * _speed);
-        //}
-        //if(Input.GetKey(KeyCode.A))
-        //{
-        //    transform.position += transform.TransformDirection(Vector3.right * Time.deltaTime * _speed);
-        //}
-        //if(Input.GetKey(KeyCode.D))
-        //{
-        //    transform.position += transform.TransformDirection(Vector3.left * Time.deltaTime * _speed);
-        //}
-
-        // Local
         if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(Vector3.forward * Time.deltaTime * _speed);
@@ -50,19 +60,5 @@ public class PlayerController : MonoBehaviour
         {
             transform.Translate(Vector3.right * Time.deltaTime * _speed);
         }
-
-        //// Idea
-        //Move(KeyCode.W, Vector3.forward);
-        //Move(KeyCode.S, Vector3.back);
-        //Move(KeyCode.A, Vector3.left);
-        //Move(KeyCode.D, Vector3.right);
-
-        //void Move(KeyCode key, Vector3 vec)
-        //{
-        //    if(Input.GetKey(key))
-        //    {
-        //        transform.Translate(vec * Time.deltaTime * _speed);
-        //    }
-        //}
     }
 }
